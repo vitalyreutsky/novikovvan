df8c97bcdc6fea637f8b72186d88015bea4e6681
Vitaly Reutsky <79563766+vitalyreutsky@users.noreply.github.com>
2024-07-31 16:35:43 +0300

    init project

---
+++ assets/themes/novikovvan/templates/blocks/hero.php
@@ -0,0 +1,26 @@
+<?php
+$title = get_field('title');
+$desc = get_field('description');
+$image = get_field('image');
+?>
+
+<div class="hero" style="background-image: url('<?php echo wp_get_attachment_url($image); ?>');">
+   <div class="container hero__container">
+      <div class="hero__wrapper">
+         <div class="hero__info">
+            <?php if ($title) : ?>
+               <h1 class="hero__title main-title"><?php echo $title; ?></h1>
+            <?php endif; ?>
+
+            <?php if ($desc) : ?>
+               <p class="hero__desc"><?php echo $desc; ?></p>
+            <?php endif; ?>
+
+            <a href="#" class="hero__btn btn">
+               <span></span>
+               Подробнее
+            </a>
+         </div>
+      </div>
+   </div>
+</div>
---
+++ assets/themes/novikovvan/templates/components/header.php
@@ -0,0 +1,49 @@
+<?php
+$site_logo = get_field('site_logo', 'options');
+?>
+
+<header class="header">
+  <div class="container">
+    <div class="header__wrapper">
+      <?php if ($site_logo) : ?>
+        <a href="/" class="header__logo">
+          <?php echo App\Base\SvgSupport::get_inline_svg_by_id($site_logo); ?>
+        </a>
+      <?php endif; ?>
+
+      <nav class="header__nav nav" title="nav-menu">
+        <ul class="list-reset nav__list">
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              <span>Обо мне</span>
+            </a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              <span>Отзывы</span>
+            </a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              <span>Программы тренировок</span>
+            </a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              <span>Контакты</span>
+            </a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              <span>Вопросы</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
