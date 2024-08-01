7a789d2d89b33ae36c932fb2fe0397a4cc162ced
Vitaly Reutsky <79563766+vitalyreutsky@users.noreply.github.com>
2024-08-01 17:17:47 +0300

    fix about block

--- assets/themes/novikovvan/templates/blocks/about.php
+++ assets/themes/novikovvan/templates/blocks/about.php
@@ -1,39 +1,46 @@
 <?php
 $title = get_field('title');
 $desc = get_field('description');
-$bg_image = get_field('bg_image');
 $about_achievements = get_field('about_achievements');
+$desc = get_field('description');
 ?>
 
 <section class="about bg-block">
    <div class="container about__container">
-      <div class="about__wrapper">
+      <div class="about__inner">
          <?php if ($title) : ?>
             <h1 class="about__title main-title"><?php echo $title; ?></h1>
          <?php endif; ?>
-      </div>
 
-      <div class="achievements__slider" data-sliders-count="<?php echo count($about_achievements); ?>">
-         <div class="achievements__items">
-            <?php foreach ($about_achievements as $item) :
-            ?>
-               <div class="achievements__item">
-                  <?php echo wp_get_attachment_image($item['image'], 'full') ?>
 
-                  <p class="sliders__item-text">
-                     <?php echo $item['text']; ?>
-                  </p>
-               </div>
-            <?php endforeach; ?>
+         <div class="achievements__slider" data-sliders-count="<?php echo count($about_achievements); ?>">
+            <div class="achievements__items">
+               <?php foreach ($about_achievements as $item) :
+               ?>
+                  <div class="achievements__item">
+                     <?php echo wp_get_attachment_image($item['image'], 'full') ?>
+
+                     <p class="sliders__item-text">
+                        <?php echo $item['text']; ?>
+                     </p>
+                  </div>
+               <?php endforeach; ?>
+            </div>
+
+            <ul class="achievements__pagination list-reset">
+               <?php foreach ($about_achievements as $item) : ?>
+                  <li class="achievements__pagination-item">
+                     <div class="pagination__button"></div>
+                  </li>
+               <?php endforeach; ?>
+            </ul>
          </div>
-
-         <ul class="achievements__pagination list-reset">
-            <?php foreach ($about_achievements as $item) : ?>
-               <li class="achievements__pagination-item">
-                  <div class="pagination__button"></div>
-               </li>
-            <?php endforeach; ?>
-         </ul>
       </div>
+
+      <?php if ($desc) : ?>
+         <div class="about__description">
+            <?php echo $desc; ?>
+         </div>
+      <?php endif; ?>
    </div>
 </section>
