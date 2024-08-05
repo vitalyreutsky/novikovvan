c1a28380b1d90e1e6a17a109c35284ac97a932ba
Vitaly Reutsky <79563766+vitalyreutsky@users.noreply.github.com>
2024-08-05 14:20:07 +0300

    fix program list

--- assets/themes/novikovvan/templates/blocks/programs-list.php
+++ assets/themes/novikovvan/templates/blocks/programs-list.php
@@ -9,7 +9,7 @@ $programs_list = get_field('programs_list');
             <h1 class="program-list__title main-title" data-aos="zoom-in-up"><?php echo $title; ?></h1>
          <?php endif; ?>
 
-         <div class="program-list__cards swiper">
+         <div class="program-list__cards swiper" data-aos="zoom-in-up">
             <div class="program-list__cards-wrapper swiper-wrapper">
                <?php foreach ($programs_list as $program) {
                   get_template_part('templates/cards/program-card', null, array('program_id' => $program));
