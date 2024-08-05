f5bba0301fe93d64f6b66e3ce1e22723c343f78d
Vitaly Reutsky <79563766+vitalyreutsky@users.noreply.github.com>
2024-08-05 09:46:36 +0300

    fix look block

--- assets/themes/novikovvan/templates/blocks/look.php
+++ assets/themes/novikovvan/templates/blocks/look.php
@@ -1,6 +1,7 @@
 <?php
 $title  = get_field('title');
 $phone_image = get_field('phone_image');
+$phone_image_bg = get_field('phone_image_bg');
 $figure_image = get_field('figure_image');
 $bg_image = get_field('bg_image');
 $types = get_field('types');
@@ -10,11 +11,11 @@ $types = get_field('types');
    <div class="container">
       <div class="look__wrapper">
          <?php if ($title) : ?>
-            <h2 class="look__title main-title"><?php echo $title; ?></h2>
+            <h2 class="look__title main-title" data-aos="zoom-in-up"><?php echo $title; ?></h2>
          <?php endif; ?>
 
          <div class="look__inner">
-            <div class="look__program" style="background-image: url('<?php echo wp_get_attachment_url($phone_image); ?>');">
+            <div class="look__program" style="background-image: url('<?php echo wp_get_attachment_url($phone_image); ?>'); --phone-bg: url('<?php echo wp_get_attachment_url($phone_image_bg); ?>');" data-aos="fade-right">
                <div class="look__program-inner">
                   <div class="swiper look__program-slider">
                      <div class="swiper-wrapper look__program-slider-wrapper">
@@ -30,10 +31,15 @@ $types = get_field('types');
                   <div class="look__program-btns">
                   </div>
                </div>
+
+               <a href="#programs" class="look__btn btn navigation__item">
+                  <span></span>
+                  Перейти к программам
+               </a>
             </div>
 
             <?php if ($figure_image) : ?>
-               <div class="look__image">
+               <div class="look__image" data-aos="fade-left">
                   <?php echo wp_get_attachment_image($figure_image, 'full'); ?>
                </div>
             <?php endif; ?>
